fb9c67617cbbaf52e00e205e1435c8d15230987a
Liu Ho Yin <j-lhy@hotmail.com>
2013-09-16 09:02:52 +0800

    improve the algo, algo generate a normal ordered table, which does not required extra sorting

--- Truthtable.hs
+++ Truthtable.hs
@@ -7,10 +7,11 @@ type TruthTable = [[Bool]]
 -- Input: current row, current column,  how many rows for t's pair or f's pair 
 -- Output: True values in a row
 rowValues :: Integer -> Integer -> Integer-> [Bool]
-rowValues _ (-1) _ = []
-rowValues i j n = let previousColumns = rowValues i (j-1) (n*2)
-                      truthValue      =  even (div i n)
-                  in  reverse (truthValue : previousColumns)
+rowValues i j n = reverse (rowValues' i j n)
+                where rowValues' _ (-1) _ = []
+                      rowValues' i j n = let previousColumns = rowValues' i (j-1) (n*2)
+                                             truthValue      =  even (div i n)
+                                         in  truthValue : previousColumns
                   
 -- Build a truth table with n variables
 -- Input: the number of variable
@@ -18,8 +19,7 @@ rowValues i j n = let previousColumns = rowValues i (j-1) (n*2)
 truthTable :: Integer -> TruthTable
 truthTable n = let lastRowIndex = 2^n-1
                    lastColumnIndex = n-1
-                   unsortedTable = map (\x -> rowValues x (lastColumnIndex) 1) [0..lastRowIndex]
-               in  reverse (sort unsortedTable)
+               in  map (\x -> rowValues x (lastColumnIndex) 1) [0..lastRowIndex]
 
 -- Apply the function to each row of a truth table, and return the result 
 -- Input : function going to be applied, number of variable in the truth table
